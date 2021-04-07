20b1ce3d50b5323cf9ff981da52b77b232034d2c
Saul Yu <saulxaviercyu@gmail.com>
2021-04-07 18:39:24 +0100

    added timestamps to server.py
    using datetime;

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import socket
 import threading
+from datetime import datetime
 
 
-HOST = "2001:818:e2c1:bf00:b5ba:858a:3dbc:9150" # Server IP address
+HOST = "2001:818:e2c1:bf00:7929:aba0:9b56:7a62" # Server IP address
 PORT = 1234
 ADDRESS = (HOST, PORT)
 
 MESSAGE_SIZE = 1024
 ENCODING = 'utf-8'
 DISCONNECT_MESSAGE = "!!!!!" # Not used yet
+PING_MESSAGE = "ping"
 
 CLIENTS = []
 client_count = 0
@@ -18,7 +20,8 @@ s_socket.bind(ADDRESS)
 
 
 def server_start():
-    print(f"Server {ADDRESS}")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"[{current_time}] Server {ADDRESS} online")
     s_socket.listen()
 
     thread_broadcast = threading.Thread(target=server_broadcast, args=())
@@ -31,7 +34,8 @@ def server_start():
 
 
 def client_connect(connection, address):
-    print(f"Client {address} connected to server.")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"[{current_time}] Client {address} connected to server.")
     CLIENTS.append(connection)
     global client_count
     client_count += 1
@@ -39,21 +43,29 @@ def client_connect(connection, address):
     while True:
         message = connection.recv(MESSAGE_SIZE).decode(ENCODING)
         if (message):
-            if (message == DISCONNECT_MESSAGE):
-                print(f"Client {address} disconnected from server.")
-                connection.close()
-                CLIENT.remove(connection)
-                client_count -= 1
-                break
-            print(f"{address}: {message}")
-            connection.send("Yes!".encode(ENCODING))
+            # if (message == DISCONNECT_MESSAGE):
+            #     print(f"Client {address} disconnected from server.")
+            #     connection.close()
+            #     CLIENT.remove(connection)
+            #     client_count -= 1
+            #     break
+            current_time = datetime.now().strftime("%H:%M:%S")
+            print(f"[{current_time}] Client {address}: {message}")
+            if (message == PING_MESSAGE):
+                response = '{"message": "pong", "priority": 1}'
+                connection.send(response.encode(ENCODING))
+                current_time = datetime.now().strftime("%H:%M:%S")
+                print(f"[{current_time}] Server: pong")
+
 
 
 def server_broadcast():
     while True:
-        dummy_message = input()
+        message = input()
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print(f"[{current_time}] Server: {message}")
         for connection in CLIENTS:
-            connection.send(dummy_message.encode(ENCODING))
+            connection.send(message.encode(ENCODING))
 
 
 if __name__ == '__main__':
