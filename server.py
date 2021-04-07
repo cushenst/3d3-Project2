59869acd5a6e1e9fd2f3747782f2a5a8d1d7fc61
Saul Yu <saulxaviercyu@gmail.com>
2021-04-07 18:50:33 +0100

    added error handling for disconnections

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ ADDRESS = (HOST, PORT)
 
 MESSAGE_SIZE = 1024
 ENCODING = 'utf-8'
-DISCONNECT_MESSAGE = "!!!!!" # Not used yet
 PING_MESSAGE = "ping"
 
 CLIENTS = []
@@ -21,7 +20,7 @@ s_socket.bind(ADDRESS)
 
 def server_start():
     current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"[{current_time}] Server {ADDRESS} online")
+    print(f"[{current_time}] Server {ADDRESS} online.")
     s_socket.listen()
 
     thread_broadcast = threading.Thread(target=server_broadcast, args=())
@@ -41,21 +40,28 @@ def client_connect(connection, address):
     client_count += 1
     print(f"Active Connections: {client_count}")
     while True:
-        message = connection.recv(MESSAGE_SIZE).decode(ENCODING)
-        if (message):
-            # if (message == DISCONNECT_MESSAGE):
-            #     print(f"Client {address} disconnected from server.")
-            #     connection.close()
-            #     CLIENT.remove(connection)
-            #     client_count -= 1
-            #     break
-            current_time = datetime.now().strftime("%H:%M:%S")
-            print(f"[{current_time}] Client {address}: {message}")
-            if (message == PING_MESSAGE):
-                response = '{"message": "pong", "priority": 1}'
-                connection.send(response.encode(ENCODING))
+        try:
+            message = connection.recv(MESSAGE_SIZE).decode(ENCODING)
+            if (message):
+                # if (message == DISCONNECT_MESSAGE):
+                #     print(f"Client {address} disconnected from server.")
+                #     connection.close()
+                #     CLIENT.remove(connection)
+                #     client_count -= 1
+                #     break
                 current_time = datetime.now().strftime("%H:%M:%S")
-                print(f"[{current_time}] Server: pong")
+                print(f"[{current_time}] Client {address}: {message}")
+                if (message == PING_MESSAGE):
+                    response = '{"message": "pong", "priority": 1}'
+                    connection.send(response.encode(ENCODING))
+                    current_time = datetime.now().strftime("%H:%M:%S")
+                    print(f"[{current_time}] Server: pong")
+        except ConnectionResetError:
+            print(f"Client {address} disconnected from server.")
+            connection.close()
+            CLIENTS.remove(connection)
+            client_count -= 1
+            break
 
 
 
