a6ad7446fdc84a2c7b1af9af5a654fb9776a0fb6
Saul Yu <saulxaviercyu@gmail.com>
2021-04-13 11:15:17 +0100

    added terminal commands

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ import socket
 import threading
 import time
 import json
-from routes import json_var
 
-HOST = "127.0.0.1" # Server IP address
+
+HOST = "2001:818:e2c1:bf00:4414:e8a3:97fb:fd77" # Server IP address
 PORT = 1234
 ADDRESS = (HOST, PORT)
 
@@ -15,7 +15,7 @@ PING_MESSAGE = "ping"
 CLIENTS = []
 client_count = 0
 
-s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 s_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s_socket.bind(ADDRESS)
 
@@ -25,13 +25,28 @@ def server_start():
     print(f"[{current_time}] Server {ADDRESS} online.")
     s_socket.listen()
 
-    thread_broadcast = threading.Thread(target=server_broadcast, args=())
-    thread_broadcast.start()
+    thread_commands = threading.Thread(target=server_commands, args=())
+    thread_commands.setDaemon(True)
+    thread_commands.start()
 
     while True:
         connection, address = s_socket.accept()
         thread_connection = threading.Thread(target=client_connect, args=(connection, address))
+        thread_connection.setDaemon(True)
         thread_connection.start()
+
+
+def server_commands():
+    while True:
+        command = input()
+        if (command == "broadcast"):
+            server_broadcast()
+        elif (command == "connections"):
+            server_connections()
+        elif (command == "help"):
+            server_help()
+        else:
+            print("Command not found. Try in \"help\".")
 
 
 def client_connect(connection, address):
@@ -45,12 +60,6 @@ def client_connect(connection, address):
         try:
             message = connection.recv(MESSAGE_SIZE).decode(ENCODING)
             if (message):
-                # if (message == DISCONNECT_MESSAGE):
-                #     print(f"Client {address} disconnected from server.")
-                #     connection.close()
-                #     CLIENT.remove(connection)
-                #     client_count -= 1
-                #     break
                 current_time = time.strftime("%x %X")
                 print(f"[{current_time}] Client {address}: {message}")
                 if (message == PING_MESSAGE):
@@ -68,19 +77,32 @@ def client_connect(connection, address):
             break
 
 
-
 def server_broadcast():
-    while True:
-        message = json_var["message"]
-        priority = json_var["priority"]
-        json_message = {"message": message, "priority": priority}
-        json_message = json.dumps(json_message)
+    print("Enter message: ")
+    message = input()
+    print("Enter priority: ")
+    priority = int(input())
+    json_message = {"message": message, "priority": priority}
+    json_message = json.dumps(json_message)
 
-        for connection in CLIENTS:
-            connection.send(json_message.encode(ENCODING))
+    for connection in CLIENTS:
+        connection.send(json_message.encode(ENCODING))
 
-        current_time = time.strftime("%x %X")
-        print(f"[{current_time}] Server: {json_message}")
+    current_time = time.strftime("%x %X")
+    print(f"[{current_time}] Server: {json_message}")
+
+
+def server_connections():
+    current_time = time.strftime("%x %X")
+    print(f"[{current_time}] Server: {client_count} active connections.")
+    for connection in CLIENTS:
+        print(connection)
+
+
+def server_help():
+    print("\"broadcast\" - broadcasts message and priority to all active connections.")
+    print("\"connections\" - shows the number of active connections and lists them.")
+    print("\"help\" - lists all acceptable commands.")
 
 
 if __name__ == '__main__':
